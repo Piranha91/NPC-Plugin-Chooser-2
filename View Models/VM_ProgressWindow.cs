47f7ca060b10cd54519481fd4ea98354d78daa43
A Piranha <piranha9191@gmail.com>
2025-10-09 00:59:30 -0700

    Implemented bulk deletion for autogen mugshots

---
+++ View Models/VM_ProgressWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Disposables;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace NPC_Plugin_Chooser_2.View_Models;
+
+public class VM_ProgressWindow : ReactiveObject, IDisposable
+{
+    private readonly CompositeDisposable _disposables = new();
+
+    [Reactive] public string Title { get; set; } = "Progress";
+    [Reactive] public string StatusMessage { get; set; } = "Processing...";
+    [Reactive] public double ProgressValue { get; set; } = 0;
+    [Reactive] public double ProgressMaximum { get; set; } = 100;
+    [Reactive] public bool IsIndeterminate { get; set; } = false;
+    [Reactive] public bool IsCancellationRequested { get; private set; } = false;
+    [Reactive] public bool CanCancel { get; set; } = true;
+
+    public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+
+    public VM_ProgressWindow()
+    {
+        CancelCommand = ReactiveCommand.Create(() =>
+        {
+            IsCancellationRequested = true;
+            CanCancel = false; // Disable the button after cancellation
+            StatusMessage = "Cancelling...";
+        }, this.WhenAnyValue(x => x.CanCancel)).DisposeWith(_disposables);
+    }
+
+    public void UpdateProgress(double value, string? message = null)
+    {
+        ProgressValue = value;
+        if (message != null)
+        {
+            StatusMessage = message;
+        }
+    }
+
+    public void Dispose()
+    {
+        _disposables.Dispose();
+    }
+}
