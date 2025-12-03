29772f04ee81a65c4f004c68b3b5559bef9866ed
A Piranha <piranha9191@gmail.com>
2025-12-02 22:53:30 -0800

    Version bump

--- App.xaml.cs
+++ App.xaml.cs
@@ -29,7 +29,7 @@ namespace NPC_Plugin_Chooser_2
     {
         private SplashScreenWindow _splashScreenWindow;
         private IContainer _container;
-        public const string ProgramVersion = "2.0.8"; // Central version definition
+        public const string ProgramVersion = "2.0.9"; // Central version definition
 
         // App constructor should be minimal
         public App()
